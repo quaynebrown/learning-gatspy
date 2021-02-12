05a75ee2d7d551f82b2cf3d2d9e1c5325070c686
quaynebrown <quayne.brown@gmail.com>
2021-02-12 14:40:48 -0500

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -36,7 +36,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Hazza`,
 }
 
 export default Header
---
+++ src/pages/inputPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const inputPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default inputPage;
